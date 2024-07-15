69b82b24f566b052b98431b9cfe677bba1575977
Max Vigueras Tovar <mvt.2000@hotmail.com>
2024-07-14 23:39:03 -0600

    Components: Button Example Added

--- CursoiOS/Components/ButtonExample.swift
+++ CursoiOS/Components/ButtonExample.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ButtonExample: View {
     var body: some View {
         Button("Hola") {
-                print("Hola")
+            print("Hola")
             
         }
         .frame( width:100, height: 50)
@@ -18,16 +18,40 @@ struct ButtonExample: View {
         .background(.blue)
         .cornerRadius(40)
         
-        Button(action: {
-            print("Hola")
-        }, label: {
-           Image("swift")
-                .resizable()
-                .frame(width: 100, height: 100)
-        })
+        Button(
+            action: {
+                print("Hola")
+            },
+            label: {
+                Image("swift")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+            })
     }
 }
 
+struct Counter: View {
+    @State var suscribersNumber = 0
+    var body: some View {
+        Button(
+            action: {
+                suscribersNumber += 1
+            },
+            label: {
+                Text("Suscriptores: \(suscribersNumber)")
+                    .bold()
+                    .font(.title)
+                    .frame( height: 50)
+                    .foregroundColor(.white)
+                    .background(.red)
+                    .cornerRadius(10)
+                    
+            })
+        
+    }
+}
+
+
 #Preview {
-    ButtonExample()
+    Counter()
 }
